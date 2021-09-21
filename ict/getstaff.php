e215213cc43fb21339b71300eda7f8db10c1e62c
peteradeojo <peteradeojo@outlook.com>
2021-09-22 00:22:13 +0100

    breaking change

--- ict/getstaff.php
+++ ict/getstaff.php
@@ -9,13 +9,13 @@ if ($people) {
 	}
 }
 
-for ($i = 0; $i < count($people); $i += 1) {
-	$dept = $db->select('departments', where: "short_code='{$people[$i]['department']}'")['name'];
-	$active = $db->select('login', rows: 'active', where: "username='{$people[$i]['username']}'")['active'];
-	// echo $dept;
-	$people[$i]['name'] = "{$people[$i]['lastname']} {$people[$i]['firstname']}";
-	$people[$i]['department'] = $dept;
-	$people[$i]['active'] = $active;
-}
+// for ($i = 0; $i < count($people); $i += 1) {
+// 	$dept = $db->select('departments', where: "short_code='{$people[$i]['department']}'")['name'];
+// 	$active = $db->select('login', rows: 'active', where: "username='{$people[$i]['username']}'")['active'];
+// 	// echo $dept;
+// 	$people[$i]['name'] = "{$people[$i]['lastname']} {$people[$i]['firstname']}";
+// 	$people[$i]['department'] = $dept;
+// 	$people[$i]['active'] = $active;
+// }
 
 echo json_encode($people);
