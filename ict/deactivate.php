85ea91186115be94581cbd554915c0b31f373e34
peteradeojo <peteradeojo@outlook.com>
2021-09-22 01:57:34 +0100

    safe deactivation

--- ict/deactivate.php
+++ ict/deactivate.php
@@ -5,6 +5,12 @@ require '../init.php';
 $data = file_get_contents("php://input");
 $user = json_decode($data);
 if ($user) {
+	// echo $staff->getUsername();
+	// exit();
+	if ($user->username == $staff->getUsername()) {
+		echo json_encode(['message' => 'Cannot deactivate your own account']);
+		exit();
+	}
 	try {
 		//code...
 		$db->update([
