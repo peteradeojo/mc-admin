0fb6a9d77ba143ef0421c524aacbecb94df8f8f2
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-01-09 09:43:35 +0100

    run migration

--- init.php
+++ init.php
@@ -111,6 +111,33 @@ $dotenv->load();
 // 	});
 // });
 
+(function () {
+	Migration::createTableIfNotExists('antenatal_visits', function (Builder $table) {
+		$table->id();
+
+		$table->integer('session_id');
+		$table->foreign('session_id', 'antenatal_sessions');
+
+		$table->integer('checked_in_by');
+		$table->foreign('checked_in_by', 'staff');
+
+		$table->integer('attending_nurse_id', false);
+		$table->foreign('attending_nurse_id', 'staff');
+
+		$table->json('vitals', false);
+
+		$table->integer('doc_id', false);
+		$table->foreign('doc_id', 'staff');
+
+		$table->datetime('date_submitted', false)->default('CURRENT_TIMESTAMP');
+		$table->timestamps();
+	});
+
+	Migration::alterTable('antenatal_visits', function (Builder $table) {
+		$table->integer('status', false)->default(0)->add('doc_id');
+	});
+})();
+
 if (@$_ENV['maintenance'] == 1) {
 	echo "Under maintenance. Try again later";
 	exit();
