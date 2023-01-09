077653a884a6f6562c1effcabb6e6525ad8eded8
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-01-09 05:12:50 +0100

    run migrations

--- init.php
+++ init.php
@@ -16,123 +16,100 @@ require 'src/functions.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-function runMigration()
-{
-
-	Migration::createTableIfNotExists('departments', function (Builder $table) {
-		$table->id();
-		$table->string('name', 40);
-		$table->string('code', 3);
-		$table->timestamps();
-	});
-
-	Migration::createTableIfNotExists('staff', function (Builder $table) {
-		$table->id();
-		$table->string('firstname', 20);
-		$table->string('lastname', 20);
-		$table->string('username');
-		$table->string('phone_number', 16, false);
-		$table->string('official_address', 50, false);
-		$table->string('designation', 30);
-		$table->integer('department', false);
-		$table->foreign('department', 'departments');
-		$table->timestamps();
-	});
-
-	Migration::createTableIfNotExists('login', function (Builder $table) {
-		$table->id();
-		$table->integer('staff_id');
-		$table->foreign('staff_id', 'staff', 'CASCADE', 'RESTRICT');
-		$table->string('password', 60);
-		$table->boolean('readwrite')->default(false);
-		$table->integer('accesslevel')->default(1);
-		$table->boolean('active')->default(true);
-		$table->timestamps();
-	});
-
-	Migration::createTableIfNotExists('categories', function (Builder $table) {
-		$table->id();
-		$table->string('name', 20);
-		$table->string('short_code', 4);
-		$table->unique('name');
-		$table->timestamps();
-	});
-
-	Migration::createTableIfNotExists('patients', function (Builder $table) {
-		$table->id();
-		$table->string('hospital_number', 20);
-		$table->string('name', 60);
-		$table->integer('gender', false)->default(0);
-		$table->integer('category_id', false)->default(1);
-		$table->foreign('category_id', 'categories');
-		$table->timestamps();
-	});
-
-	Migration::createTableIfNotExists('biodata', function (Builder $table) {
+(function () {
+	Migration::createTableIfNotExists('antenatal_sessions', function (Builder $table) {
 		$table->id();
 		$table->integer('patient_id');
-		$table->foreign('patient_id', 'patients', 'CASCADE', 'RESTRICT');
-		$table->string('address', 50, false);
-		$table->string('email_address', 50, false);
-		$table->string('phone_number', 16, false);
-		$table->string('occupation', 30, false);
-		$table->string('marital_status', 10, false);
-		$table->string('religion', 10, false);
-	});
+		$table->foreign('patient_id', 'biodata');
 
-	Migration::createTableIfNotExists('vitals', function (Builder $table) {
-		$table->id();
-		$table->integer('patient_id');
-		$table->foreign('patient_id', 'patients', 'CASCADE', 'RESTRICT');
-		$table->integer('staff_id');
-		$table->foreign('staff_id', 'staff', 'CASCADE', 'RESTRICT');
-		$table->string('temp', 10, false);
-		$table->integer('pulse', 10, false);
-		$table->string('resp', 10, false);
-		$table->string('bp', 10, false);
-		$table->decimal('weight', 3, 2, false);
+		$table->date('flmp', false);
+		$table->date('edd', false);
+
+		$table->string('delivery_status', 30, false);
 		$table->timestamps();
 	});
 
-	Migration::createTableIfNotExists('statuses', function (Builder $table) {
+	Migration::createTableIfNotExists('anc_session_personal_history', function (Builder $table) {
 		$table->id();
-		$table->string('title', 20);
-		$table->unique('title');
+		$table->integer('anc_session_id');
+		$table->foreign('anc_session_id', 'antenatal_sessions');
+
+		$table->boolean('chest_disease', false)->default(false);
+		$table->boolean('kidney_disease', false)->default(false);
+		$table->boolean('blood_transfusion', false)->default(false);
+		$table->boolean('operation_no_cs', false)->default(false);
+		$table->string('others', 255, false)->default('');
 		$table->timestamps();
 	});
 
-	Migration::createTableIfNotExists('waitlist', function (Builder $table) {
+	Migration::createTableIfNotExists('anc_session_family_history', function (Builder $table) {
 		$table->id();
-		$table->integer('patient_id');
-		$table->foreign('patient_id', 'patients', 'CASCADE', 'RESTRICT');
-		$table->integer('staff_id');
-		$table->foreign('staff_id', 'staff', 'CASCADE', 'RESTRICT');
-		$table->integer('vital_id', false);
-		$table->foreign('vital_id', 'vitals', 'CASCADE', 'RESTRICT');
-		$table->integer('status_id')->default(1);
-		$table->foreign('status_id', 'statuses');
+		$table->integer('anc_session_id');
+		$table->foreign('anc_session_id', 'antenatal_sessions');
+
+		$table->boolean('multiple_births', false)->default(false);
+		$table->string('multiple_births_relation', 30, false);
+		$table->boolean('hypertension', false)->default(false);
+		$table->string('hypertension_relation', 30, false);
+		$table->boolean('tuberculosis', false)->default(false);
+		$table->string('tuberculosis_relation', 30, false);
+		$table->boolean('heart_diseases', false)->default(false);
+		$table->string('heart_disease_relation', 30, false);
+		$table->string('others', 100, false);
+		$table->string('others_relations', 100, false);
 		$table->timestamps();
 	});
-}
 
-function alterVisitsTable()
-{
-	// Migration::alterTable('visits', function (Builder $table) {
-	// 	$table->modify('admission_id', 'varchar(50)');
-	// 	$table->modify('lab_tests_id', 'varchar(50)');
-	// 	$table->modify('investigations_id', 'varchar(50)');
-	// 	$table->modify('review_id', 'varchar(50)');
-	// });
+	Migration::createTableIfNotExists('anc_session_objective_history', function (Builder $table) {
+		$table->id();
+		$table->integer('anc_session_id');
+		$table->foreign('anc_session_id', 'antenatal_sessions');
 
-	Migration::alterTable('lab_tests', function (Builder $table) {
-		$table->modify('id', 'varchar(50) not null primary key');
-		$table->modify('date', 'datetime not null');
-		$table->integer('status')->add('date');
-		$table->modify('status', 'int not null default 0'); //->add('date');
+		$table->integer('gravidity');
+		$table->integer('parity');
+		$table->timestamps();
 	});
-}
 
-// alterVisitsTable();
+	Migration::createTableIfNotExists('anc_session_pregnancy_history', function (Builder $table) {
+		$table->id();
+		$table->integer('anc_session_id');
+		$table->foreign('anc_session_id', 'antenatal_sessions');
+
+		$table->date('date_of_delivery', false);
+		$table->string('delivery_type', 30, false);
+		$table->string('delivery_place', 30, false);
+		$table->string('duration_of_pregnancy', 20, false);
+		$table->integer('living_status', false)->default(1);
+		$table->string('plp', 20, false);
+		$table->boolean('gender', false)->default(true);
+
+		$table->timestamps();
+	});
+
+	Migration::createTableIfNotExists('anc_session_current_history', function (Builder $table) {
+		$table->id();
+		$table->integer('anc_session_id');
+		$table->foreign('anc_session_id', 'antenatal_sessions');
+
+		$table->decimal('weight', 5, 2, false);
+		$table->decimal('height', 5, 2, false);
+		$table->string('bp', 9, false);
+
+		$table->string('vaginal_bleeding', 20, false);
+		$table->string('vaginal_discharge', 20, false);
+		$table->string('urinary_symptoms', 20, false);
+		$table->string('leg_swelling', 20, false);
+		$table->string('other_symptoms', 60, false);
+
+		$table->timestamps();
+	});
+})();
+
+(function () {
+	Migration::alterTable('antenatal_sessions', function (Builder $table) {
+		$table->modify('delivery_status', 'int not null default 0');
+	});
+})();
 
 if (@$_ENV['maintenance'] == 1) {
 	echo "Under maintenance. Try again later";
