c3f1342eb6230a1267ebbf23dfa4e88a0f8b3bb8
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-01-09 08:43:41 +0100

    last commit

--- rec/getpatients.php
+++ rec/getpatients.php
@@ -8,7 +8,7 @@ $patients = $db->join(['biodata as bio', 'antenatal_sessions as ancs'], [
 		'type' => 'left',
 		'on' => 'bio.id = ancs.patient_id'
 	],
-], table_rows: "bio.*, ancs.flmp, ancs.edd, ancs.delivery_status, ancs.id as anc_id");
+], table_rows: "bio.*, ancs.flmp, ancs.edd, ancs.delivery_status, ancs.id as anc_id", limit: null);
 
 $patients = array_map(function ($patient) {
 	$patient['category'] = ucfirst($patient['category']);
